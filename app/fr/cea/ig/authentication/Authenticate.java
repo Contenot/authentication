bbbb676cfde67fb7e89394777f562cb6e60c9a32
contenot <sandrine.contenot@inra.fr>
2017-05-19 10:59:04 +0200

    test sandrine 1905

--- app/fr/cea/ig/authentication/Authenticate.java
+++ app/fr/cea/ig/authentication/Authenticate.java
@@ -1,6 +1,5 @@
 package fr.cea.ig.authentication;
-///Sc jkdfqd
-
+///Sc jkdfqd//dsqfdqsfdsfdsqfdqfdsq
 ///teste kdjqfkdjkqsf
 import fr.cea.ig.authentication.activedirectory.ActiveDirectoryAuthentication;
 import fr.cea.ig.authentication.activedirectory.ActiveDirectoryPlugin;
